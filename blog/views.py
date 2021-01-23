6226bf52697b1f746453b3de053e6dbedb6d3975
sonam yadav <75215017+sonamyadav0088@users.noreply.github.com>
2021-01-24 02:24:51 +0530

    add urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Blog
 
-# Create your views here.
+class BlogList(generic.ListView):
+    queryset = Blog.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class BlogDetail(generic.DetailView):
+    model = Blog
+    template_name = 'blog_detail.html'
